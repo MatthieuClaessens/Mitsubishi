98a9e39a63652c3fe5f80aa83c3e6769d53149c6
MatthieuClaessens <matthieu-claessens@hotmail.com>
2026-08-28 21:23:47 +0200

    Created navbar

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import Mitsubishi from "@/public/image/mitsubishi.webp";
+
+export default function NavBar() {
+    return (
+        <nav className="h-16 w-full bg-black flex items-center px-8 font-sans">
+            <Image src={Mitsubishi} alt="Mitsubishi logo" priority className="w-[18px] mx-3"/>
+            <p className="font-bold text-white font-display uppercase">Lancer <span className="text-red-600">Evo</span></p>
+            <div className="flex justify-center mx-auto gap-6 uppercase font-bold text-xs tracking-[3px]">
+                <Link href="/" className="text-white">Accueil</Link>
+                <Link href="/" className="text-white">Specs</Link>
+                <Link href="/" className="text-white">L'Héritage</Link>
+            </div>
+<Link href="/configurer" className="text-white bg-red-600 px-5 py-2 uppercase font-sans font-bold text-xs tracking-[3px] hover:bg-red-700 transition-colors">Configurer</Link>            
+        </nav>
+    )
+}
